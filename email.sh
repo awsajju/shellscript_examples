53199fc889368f71b166afa24996dadefe6604d2
awsajju <aws.devops.123aws@gmail.com>
2025-01-04 19:28:54 +0530

    checking emai

--- email.sh
+++ email.sh
@@ -5,7 +5,7 @@
 echo "Enter your email"
 read email
 
-if [ $email=~^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$]
+if [ $email =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ];then
     echo "valid email"
 else
     echo "invalid email"
