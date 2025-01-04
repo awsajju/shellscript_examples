30e981074530cb0b53dde28b16290e23994c2207
awsajju <aws.devops.123aws@gmail.com>
2025-01-04 19:27:20 +0530

    checking emai

---
+++ email.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#checking email
+
+echo "Enter your email"
+read email
+
+if [ $email=~^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$]
+    echo "valid email"
+else
+    echo "invalid email"
+fi
