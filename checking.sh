93074044bc5663a3ff22c36c0a2f82bd67117716
awsajju <aws.devops.123aws@gmail.com>
2025-01-04 20:18:06 +0530

    checking emial

--- checking.sh
+++ checking.sh
@@ -5,7 +5,7 @@
 echo "Enter the emial"
 read email
 
-if [[ $emial =~ ^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]];then
+if [[ $email =~ ^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]];then
     echo "valid email"
 else
     echo "invalid email"
