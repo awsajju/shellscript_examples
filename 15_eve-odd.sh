a736f1b1aeae2945e173f8d1382b433594276c7e
awsajju <aws.devops.123aws@gmail.com>
2025-01-04 19:07:19 +0530

    15_even or odd

---
+++ 15_eve-odd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# even number and odd number
+
+echo "Enter the number"
+read Num
+
+if [ $Num / 2 -eq 0 ];then
+    echo "Entered the number is EVEN"
+else 
+    echo "Entered the number is odd"
+
+fi
