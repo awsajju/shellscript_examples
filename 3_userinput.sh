edb272faf71df95bfb5a0559e64794de5ddab87b
awsajju <aws.devops.123aws@gmail.com>
2025-01-04 18:08:48 +0530

    3_user_Input

---
+++ 3_userinput.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter your First_Name : "
+read First_Name
+
+echo "ENter your Second_Name : "
+read Second_Name
+
+Full_Name=$First_Name $Second_Name
+
+echo "My full name is $Full_Name"
