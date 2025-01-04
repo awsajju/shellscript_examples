fc2088b1819b1a072f168660dbb826486fbdfd7d
awsajju <aws.devops.123aws@gmail.com>
2025-01-04 21:49:16 +0530

    checking file writebale

--- file-writable.sh
+++ file-writable.sh
@@ -3,8 +3,16 @@
 echo "enter the fname"
 read fname
 
+<<name
 if [ -w $fname ];then
     echo "file writable"
+else
+    echo "file not worteble"
+fi
+name
+
+if [ ls -ltr | $fname ];then
+  echo "file writable"
 else
     echo "file not worteble"
 fi
